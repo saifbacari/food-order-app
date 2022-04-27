41368da16943dcc6ed718d5b5c1b57c00a34b87e
Saif Bacari <saifbacari@gmail.com>
2022-04-27 17:13:07 +0200

    first commit test

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+
+    return(
+        <button className={classes.button}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    )
+}
+
+export default HeaderCartButton;
